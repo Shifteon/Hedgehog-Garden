4d830b216c4d856236ee632a0a50cf55ebd80e78
Shifteon <bpwyatt04@gmail.com>
2021-05-02 14:18:43 -0600

    Implemented draw methods and new logic

--- hedgehog.py
+++ hedgehog.py
@@ -6,8 +6,10 @@ class Hedgehog:
         self.hunger = 500
         self.health = 100
         self.hygeine = 100
+        self.canGetNewHog = True
         # Max stats for hedgehog. Hunger, health, hygeine
         self.maxStats = (0, 500, 500)
+
     def feed(self, Food):
         if (self.hunger > 0):
             self.hunger -= Food.calories
@@ -20,11 +22,13 @@ class Hedgehog:
             self.health = 500
         if (Food.hasEffect):
             Food.applyEffect()
+
     def wash(self):
         if (self.hygeine < 500):
             self.hygeine += 20
         if (self.hygeine > 500):
             self.hygeine = 500
+
     def exercise(self):
         if (self.health < 500):
             self.health += 20
@@ -32,12 +36,16 @@ class Hedgehog:
             self.health = 500
         if (self.hygeine > 0):
             self.hygeine -= 10
+
     def draw(self):
-        pass
+        print(self.name)
+        print(" .|||||||||. \n|||||||||||||\n|||||||||||' .\\\n`||||||||||_,__o")
+
     def displayStatus(self):
         print("Health: ", self.health)
         print("Hunger: ", self.hunger)
         print("Hygeine:", self.hygeine)
+        
     def isMaxStatus(self):
         return self.hunger == self.maxStats[0] and self.health == self.maxStats[1] and self.hygeine == self.maxStats[2]
 
@@ -46,3 +54,17 @@ class SpecialHog(Hedgehog):
         super().__init__()
         self.hunger = 2000
         self.maxStats = (0, 2000, 2000)
+        self.canGetNewHog = False
+    
+    def draw(self):
+        print(self.name + ": This is a SPECIAL Hedgehog!")
+        print("""              \ / \/ \/ / ,
+           \ /  \/ \/  \/  / ,
+         \ \ \/ \/ \/ \ \/ \/ /
+       .\  \/  \/ \/ \/  \/ / / /
+      '  / / \/  \/ \/ \/  \/ \ \/ \\
+   .'     ) \/ \/ \/ \/  \/  \/ \ / \\
+  /   o    ) \/ \/ \/ \/ \/ \/ \// /
+o'_ ',__ .'   ,.,.,.,.,.,.,.,'- '%
+         // \\\\          // \\\\
+        ''  ''         ''  ''""")
