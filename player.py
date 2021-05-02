bec190127d33551952ce3ae25fabfa54468f8bac
Shifteon <bpwyatt04@gmail.com>
2021-05-02 14:19:21 -0600

    Added food

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 from hedgehog import Hedgehog
+from food import Food, Orange, Cake, Kibble
 
 class Player:
     def __init__(self, name):
         self.name = name
         hedgehog = Hedgehog()
         self.hedgehogs = [hedgehog]
-        self.food = []
+        food = Kibble()
+        self.food = [food]
         self.money = 100
