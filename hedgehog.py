7a02e57b1b31d129ae363bfdeb3cb5a4c45014ab
Shifteon <bpwyatt04@gmail.com>
2021-05-03 21:43:09 -0600

    Add "isSpecial"

--- hedgehog.py
+++ hedgehog.py
@@ -1,3 +1,8 @@
+"""
+Hedgehog and special hedgehog class.
+Author: Benjamin Wyatt
+"""
+
 from food import Food, Orange
 
 class Hedgehog:
@@ -5,12 +10,17 @@ class Hedgehog:
         self.name = "Harry"
         self.hunger = 500
         self.health = 100
-        self.hygeine = 100
+        self.hygiene = 100
         self.canGetNewHog = True
-        # Max stats for hedgehog. Hunger, health, hygeine
+        # Max stats for hedgehog. Hunger, health, hygiene
         self.maxStats = (0, 500, 500)
+        self.isSpecial = False
 
     def feed(self, Food):
+        """
+        Feed a hedgehog.
+        """
+        # Stay above 0
         if (self.hunger > 0):
             self.hunger -= Food.calories
         if (self.hunger < 0):
@@ -24,30 +34,45 @@ class Hedgehog:
             Food.applyEffect()
 
     def wash(self):
-        if (self.hygeine < 500):
-            self.hygeine += 20
-        if (self.hygeine > 500):
-            self.hygeine = 500
+        """
+        Wash a hedgehog.
+        """
+        if (self.hygiene < 500):
+            self.hygiene += 20
+        if (self.hygiene > 500):
+            self.hygiene = 500
 
     def exercise(self):
+        """
+        Exercise a hedgehog.
+        """
         if (self.health < 500):
             self.health += 20
         if (self.health > 500):
             self.health = 500
-        if (self.hygeine > 0):
-            self.hygeine -= 10
+        if (self.hygiene > 0):
+            self.hygiene -= 10
 
     def draw(self):
+        """
+        Draw a hedgehog.
+        """
         print(self.name)
         print(" .|||||||||. \n|||||||||||||\n|||||||||||' .\\\n`||||||||||_,__o")
 
     def displayStatus(self):
+        """
+        Displays a hedgehog's status.
+        """
         print("Health: ", self.health)
         print("Hunger: ", self.hunger)
-        print("Hygeine:", self.hygeine)
+        print("Hygiene:", self.hygiene)
         
     def isMaxStatus(self):
-        return self.hunger == self.maxStats[0] and self.health == self.maxStats[1] and self.hygeine == self.maxStats[2]
+        """
+        Returns True if the hedgehog has max status and False if it doesn't.
+        """
+        return self.hunger == self.maxStats[0] and self.health == self.maxStats[1] and self.hygiene == self.maxStats[2]
 
 class SpecialHog(Hedgehog):
     def __init__(self):
@@ -55,8 +80,12 @@ class SpecialHog(Hedgehog):
         self.hunger = 2000
         self.maxStats = (0, 2000, 2000)
         self.canGetNewHog = False
+        self.isSpecial = True
     
     def draw(self):
+        """
+        Draw the special hedgehog.
+        """
         print(self.name + ": This is a SPECIAL Hedgehog!")
         print("""              \ / \/ \/ / ,
            \ /  \/ \/  \/  / ,
