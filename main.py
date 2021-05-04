6652f65083087c4a439651d067c7724a175edce1
Shifteon <bpwyatt04@gmail.com>
2021-05-03 21:43:25 -0600

    More Comments

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+"""
+Main file. Everything runs from here.
+Author: Benjamin Wyatt
+"""
+
 from game import Game
 import os
 
 def cls():
+    """
+    Function to clear the console.
+    """
     os.system('cls' if os.name=='nt' else 'clear')
 
 name = input("Enter your name: ")
--- player.py
+++ player.py
@@ -1,11 +1,50 @@
-from hedgehog import Hedgehog
+"""
+Player class. Describes the player of the game.
+Author: Benjamin Wyatt
+"""
+
+from hedgehog import Hedgehog, SpecialHog
 from food import Food, Orange, Cake, Kibble
 
 class Player:
     def __init__(self, name):
         self.name = name
-        hedgehog = Hedgehog()
-        self.hedgehogs = [hedgehog]
-        food = Kibble()
-        self.food = [food]
+        # hedgehog = Hedgehog()
+        self.hedgehogs = []
+        # food = Kibble()
+        self.food = []
         self.money = 100
+
+    def update(self):
+        if self.money < 0:
+            self.money = 0
+
+    def initializeFood(self, numKibble, numCake, numOrange):
+        for i in range(numKibble):
+            kibble = Kibble()
+            self.food.append(kibble)
+        for i in range(numCake):
+            cake = Cake()
+            self.food.append(cake)
+        for i in range(numOrange):
+            orange = Orange()
+            self.food.append(orange)
+
+    def initializeHedgehogs(self, name, hunger, health, hygiene, canGetNewHog, isSpecial):
+        print(isSpecial)
+        if isSpecial == False:
+            hedgehog = Hedgehog()
+            hedgehog.name = name
+            hedgehog.hunger = hunger
+            hedgehog.health = health
+            hedgehog.hygiene = hygiene
+            hedgehog.canGetNewHog = canGetNewHog
+            self.hedgehogs.append(hedgehog)
+        else:
+            hedgehog = SpecialHog()
+            hedgehog.name = name
+            hedgehog.hunger = hunger
+            hedgehog.health = health
+            hedgehog.hygiene = hygiene
+            hedgehog.canGetNewHog = canGetNewHog
+            self.hedgehogs.append(hedgehog)
